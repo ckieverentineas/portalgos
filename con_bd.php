7cba868c810e3677849fe45963461b2f6dafabd2
ckieverentineas <ckieverentineas@yandex.ru>
2022-03-06 15:36:30 +1000

    hotfix

--- con_bd.php
+++ con_bd.php
@@ -3,8 +3,7 @@
     $user = "root"; //Пользователь в БД
     $password = ""; //Пароль пользователя БД
     $database = "portal"; //Название баз данных в БД
-    $mysqli = mysqli_connect($localhost, $user,
-                     $password, $database);
+    $mysqli = mysqli_connect($localhost, $user, $password, $database);
     /* в переменной $mysqli хранятся данные для соединения
     с базами данных*/
     //echo var_dump($mysqli);
